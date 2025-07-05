8c5eda0f0d99075f1a855a83cd86d361bdce4f90
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-07-05 17:13:19 +0200

    Updated CoursesPage.tsx

--- src/pages/CoursesPage.tsx
+++ src/pages/CoursesPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { supabase } from '../src/lib/supabase';
+import { supabase } from '/src/lib/supabase';
 import { 
   Search, 
   Filter, 
