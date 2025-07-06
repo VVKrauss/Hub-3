dbc084365cad85aff2cec3aa521a8d83f1c83a22
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-07-06 17:23:24 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,28 +22,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;// src/pages/HomePage.tsx
-// Обычная версия HomePage
-
-import Layout from '../components/layout/Layout';
-import HeroSection from '../components/home/HeroSection';
-import InfoSection from '../components/home/InfoSection';
-import EventsSection from '../components/home/EventsSection';
-import SpeakersSection from '../components/home/SpeakersSection';
-import RentSection from '../components/home/RentSection';
-import CoworkingSection from '../components/home/CoworkingSection';
-
-const HomePage = () => {
-  return (
-    <Layout>
-      <HeroSection />
-      <InfoSection />
-      <EventsSection />
-      <SpeakersSection />
-      <RentSection />
-      <CoworkingSection />
-    </Layout>
-  );
-};
-
-export default HomePage;
+export default HomePage; 
