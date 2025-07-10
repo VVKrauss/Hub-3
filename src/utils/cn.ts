202fb757f01f6cc2e2048a9ed98b2da55a0d5725
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-07-11 01:30:08 +0200

    Updated cn.ts

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -5,8 +5,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-// Если clsx и tailwind-merge не установлены, используйте эту простую версию:
-// export function cn(...classes: (string | undefined | null | false)[]): string {
-//   return classes.filter(Boolean).join(' ');
-// }
